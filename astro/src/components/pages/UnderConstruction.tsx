bb6b9a2492966c848895eda5b29dfdace34d6c09
techpixel <68567672+techpixel@users.noreply.github.com>
2026-01-26 13:22:00 -0500

    fix: update construction pages to use new system

--- astro/src/components/pages/UnderConstruction.tsx
+++ astro/src/components/pages/UnderConstruction.tsx
@@ -1,7 +1,9 @@
 import '../../styles/global.css';
-import { useState, useRef } from 'react';
+import { useRef, useState } from 'react';
 import clsx from 'clsx';
- 
+import EmailInput from '../primitives/EmailInput.tsx';
+import { openWithEmail } from '../../lib/email.ts';
+
 const FORM_URL_SIGN_UP = "https://forms.hackclub.com/campfire-signup";
 
 interface UnderConstructionProps {
@@ -15,12 +17,7 @@ function UnderConstruction({ event_name, record_id, error = "", is404 = false }:
     const [email, setEmail] = useState("");
     const emailRef = useRef<HTMLInputElement>(null);
 
-    function openWithEmail(url: string) {
-        if (!emailRef?.current?.reportValidity() || !email)
-            return;
-
-        window.open(`${url}?email=${encodeURIComponent(email)}&event=${encodeURIComponent(record_id || "")}`, "_blank");
-    }
+    const handleOpenWithEmail = (url: string) => openWithEmail(email, url, emailRef, record_id, `under-construction-${event_name.replaceAll('-', '')}`);
 
     return (
         <div className="w-full min-h-screen flex flex-col overflow-hidden relative bg-[#fca84a]">
@@ -129,21 +126,19 @@ function UnderConstruction({ event_name, record_id, error = "", is404 = false }:
                                 )}
                             >
                                 <img src="/icons/email.svg" alt="" className="w-6 h-5 flex-shrink-0 select-none" />
-                                <input
-                                    required
+                                <EmailInput
                                     ref={emailRef}
                                     value={email}
                                     onChange={e => setEmail(e.target.value)}
-                                    type="email"
-                                    className="text-[#854d16] text-2xl md:text-4xl font-bold truncate bg-transparent border-none outline-none flex-1 cursor-text font-ember-and-fire"
                                     placeholder="you@hackclub.com"
+                                    onSubmit={() => handleOpenWithEmail(FORM_URL_SIGN_UP)}
                                 />
                             </div>
 
                             <button
                                 className="bg-[#fca147] border-[5px] border-[rgba(0,0,0,0.2)] rounded-[20px] px-8 md:px-14 py-4 hover:scale-105 transition-transform w-full md:w-auto transform md:rotate-[1.5deg] shadow-[0_8px_20px_rgba(0,0,0,0.25)] cursor-pointer active:scale-95"
                                 type="button"
-                                onClick={() => openWithEmail(FORM_URL_SIGN_UP)}
+                                onClick={() => handleOpenWithEmail(FORM_URL_SIGN_UP)}
                             >
                                 <p
                                     className="text-[#8d3f34] text-3xl md:text-5xl font-normal font-dream-planner whitespace-nowrap"
